c3f52e99e90a6d0f4333ea696a5ac4ebe51587a8
Jean <Jean@Jean-2.local>
2016-06-10 00:56:27 +0800

    test

--- cocokua_heroku/static/js/phone.js
+++ cocokua_heroku/static/js/phone.js
@@ -4,9 +4,6 @@ $(function() {
   		opacity: 0.35
 	});
   });
-$("#video-btn").on('click',function(){
-	makeCall();
-});
 
 //Pubnub
 var phoneNumber = userFBID;
@@ -14,31 +11,30 @@ var phone = window.phone = PHONE({
         number        : phoneNumber,
         publish_key   : 'pub-c-9295f055-f256-4e51-9317-ba3b363a0769',
         subscribe_key : 'sub-c-7577b584-ba0a-11e5-8365-02ee2ddab7fe',
-        ssl           : true
+        ssl           : true,
+        media         : { audio : true, video : $("input[name='webcam-on']").is(":checked") },
     });
 phone.ready(function(){
-	var container = document.getElementById('videoChat');
-    	//if($(container).children('video-display-me')==null){
-    		var newId = 'video-display-me';
-        	var container = document.getElementById('videoChat');
-			var videoChat = document.getElementById('draggable');
-			var cln = videoChat.cloneNode(true);
-			cln.style.display="block";
-			$( cln ).draggable({
-		  		opacity: 0.35
-			});
-			container.appendChild(cln); 
-			$(cln).children('#video-display').attr('id',newId);
-			var video_out = $('#'+newId);
-			video_out.innerHTML = '';
-			video_out.append(phone.video);
-    	//}
+    $("#video-btn").on('click',function(){
+		makeCall();
+	});
 });
 function makeCall(){
+	//show my video
+	var container = document.getElementById('videoChat');
+	var videoDisplay = document.getElementById('video-display-me');
+	videoDisplay.style.display="block";
+	$( videoDisplay ).draggable({
+  		opacity: 0.35
+	});
+	videoDisplay.innerHTML = '';
+	videoDisplay.append(phone.video);
+
 	//members is defined in memberlist.js
     var sessions = [];
     	for (i=0;i<members.length;i++){
-    		sessions.push(phone.dial(members[i]));
+    		if(members[i]!=userFBID)
+    			sessions.push(phone.dial(members[i]));
     	}
 		sessions.forEach(function(friend){
 		    friend.connected(function(session){ /* call connected */ });
@@ -69,26 +65,3 @@ function connected(session) {
     video_out.appendChild(session.video);
     console.log("Hi!");
 }
-// phone.receive(function(session){
-
-//         // Display Your Friend's Live Video
-//         session.connected(function(session){
-//         	var newId = 'video-display-'+session.number;
-//         	var container = document.getElementById('videoChat');
-// 			var videoChat = document.getElementById('draggable');
-// 			var cln = videoChat.cloneNode(true);
-// 			cln.style.display="block";
-// 			$( cln ).draggable({
-// 		  		opacity: 0.35
-// 			});
-// 			container.appendChild(cln); 
-// 			$(cln).children('#video-display').attr('id',newId);
-
-// 			console.log(newId);
-// 			var video_out = PUBNUB.$(newId);
-// 			var video_out = PUBNUB.$('video-display-god');
-// 			video_out.innerHTML = '';
-//     		video_out.appendChild(session.video);
-//         });
-
-// });
