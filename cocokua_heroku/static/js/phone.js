d64b8c024e2d6f9ff6c5ba10b8104716e346ca13
Jean <Jean@Jean-2.local>
2016-06-09 20:35:24 +0800

    test

--- cocokua_heroku/static/js/phone.js
+++ cocokua_heroku/static/js/phone.js
@@ -5,26 +5,20 @@ $(function() {
 	});
   });
 $("#video-btn").on('click',function(){
-	VideoPhone();
+	makeCall();
 });
 
 //Pubnub
 var phoneNumber = userFBID;
-
-function VideoPhone(){
-    // The phone *number* can by any string value
-    var phone = PHONE({
+var phone = PHONE({
         number        : phoneNumber,
         publish_key   : 'pub-c-9295f055-f256-4e51-9317-ba3b363a0769',
         subscribe_key : 'sub-c-7577b584-ba0a-11e5-8365-02ee2ddab7fe',
         media         : { audio : true, video : true },
         ssl           : true
     });
-
-    // As soon as the phone is ready we can make calls
-    phone.ready(function(){
-    	//
-    	var container = document.getElementById('videoChat');
+phone.ready(function(){
+	var container = document.getElementById('videoChat');
     	if($(container).children('video-display-me')==null){
     		var newId = 'video-display-me';
         	var container = document.getElementById('videoChat');
@@ -40,18 +34,19 @@ function VideoPhone(){
 			video_out.innerHTML = '';
 			video_out.append(phone.video);
     	}
-    	//members is defined in memberlist.js
-        var sessions = [];
-        	for (i=0;i<members.length;i++){
-        		sessions.push(phone.dial(members[i]));
-        	}
-			sessions.forEach(function(friend){
-			    friend.connected(function(session){ /* call connected */ });
-			    friend.ended(function(session){     /* call ended     */ });
-			});
-    });
-    // When Call Comes In or is to be Connected
-    phone.receive(function(session){
+});
+function makeCall(){
+	//members is defined in memberlist.js
+    var sessions = [];
+    	for (i=0;i<members.length;i++){
+    		sessions.push(phone.dial(members[i]));
+    	}
+		sessions.forEach(function(friend){
+		    friend.connected(function(session){ /* call connected */ });
+		    friend.ended(function(session){     /* call ended     */ });
+		});
+}
+phone.receive(function(session){
 
         // Display Your Friend's Live Video
         session.connected(function(session){
@@ -72,5 +67,4 @@ function VideoPhone(){
     		video_out.appendChild(session.video);
         });
 
-    });
-}
+});
