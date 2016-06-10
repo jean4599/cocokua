46ffde89cb816ffa69334c559a1c0459f77cd28b
Jean <Jean@Jean-2.local>
2016-06-10 11:56:14 +0800

    test

--- cocokua_heroku/static/js/phone.js
+++ cocokua_heroku/static/js/phone.js
@@ -45,7 +45,7 @@ function connected(session) {
 	var container = document.getElementById('videoChat');
 	var videoChat = document.getElementById('draggable');
 	var id = '#'+newId;
-	if(document.getElementById(newId)==null)){
+	if(document.getElementById(newId)==null){
 
 		var cln = videoChat.cloneNode(true);
 		cln.style.display="block";
